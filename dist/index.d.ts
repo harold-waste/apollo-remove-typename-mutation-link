a64272849a344b8a5882e7f0398cad6cc1eb3a6f
StacyHaroldWaste <stacy.robert@haroldwaste.com>
2020-12-04 19:32:35 +0100

    initial commit

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="zen-observable" />
-import { ApolloLink, Operation, NextLink } from '@apollo/client';
-declare const removeTypenameFromMutation: (operation: Operation, forward: NextLink) => import("zen-observable")<import("@apollo/client").FetchResult<{
-    [key: string]: any;
-}, Record<string, any>, Record<string, any>>>;
-declare const removeTypenameFromMutationLink: ApolloLink;
-export { removeTypenameFromMutationLink, removeTypenameFromMutation, };
